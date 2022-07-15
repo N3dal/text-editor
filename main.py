28a7af4642bcb4683f3dd7b0de4a9a3163a55d3c
N3dal <nedalxzo@gmail.com>
2022-07-15 12:24:15 +0200

    set all the defaults for the program

--- main.py
+++ main.py
@@ -26,6 +26,28 @@ from tkinter.constants import END
 # [4]- add settings page to control of the font size and the last open,
 # files, and font color....etc.
 
+# Setup the Defaults.
+WIN_TITLE = "Text Editor"
+WIN_HEIGHT = 600
+WIN_WIDTH = 500
+WIN_BG = "#2C3E50"
+WIN_FG = "white"
+WIN_FONT_FAMILY = "Ubuntu"
+
+BTN_PROPERTIES = {
+    "font": (WIN_FONT_FAMILY, 10, "bold"),
+    "border": 0,
+    "borderwidth": 0,
+    "highlightbackground": "Black",
+    "highlightthickness": 0,
+    "bg": "#8E44AD",
+    "activebackground": "#9E54BD",
+    "fg": WIN_FG,
+    "activeforeground": "white",
+    "height": 1
+
+}
+
 
 def clear():
     """wipe terminal screen."""
